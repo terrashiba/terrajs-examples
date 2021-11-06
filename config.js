1be044eff618b41be01bda3b2127d1a5995c52ec
Sascha1337 <radatz.coding@gmail.com>
2021-11-06 13:00:31 +0800

    Update config.js
    decentralize it

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
   networks : {
       mainnet: {
-          provider: 'https://lcd.terra.dev',
+          provider: 'https://columbus-5--rpc--full.datahub.figment.io/apikey/110aabebcc2e83ec515eb54519f7d4f9',
           transactionLink : (hash) => `https://finder.terra.money/columbus-5/tx/${hash}`,
           walletLink : (address) => `https://finder.terra.money/columbus-5/address/${address}`,
           networkName: 'columbus-5',
